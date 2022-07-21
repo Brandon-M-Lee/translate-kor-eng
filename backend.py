03f960bdbdd01c6951b21e3c62d9e49b92b8dd6c
Brandon-M-Lee <minjae.py@gmail.com>
2022-07-21 16:41:54 +0900

    이슈 해결중

--- backend.py
+++ backend.py
@@ -36,7 +36,7 @@ def eng_to_kor(s):
             continue
         if s[i] == ' ':
             kor_str += ' '
-        elif s[i] == 'h':
+        if s[i] == 'h' and i < len(s)-1:
             if s[i+1] == 'k':
                 kor_str += kor_index[eng2krindex['hk']]
                 flag = True
@@ -48,7 +48,7 @@ def eng_to_kor(s):
                 flag = True
             else:
                 kor_str += kor_index[eng2krindex['h']]
-        elif s[i] == 'n':
+        elif s[i] == 'n' and i < len(s)-1:
             if s[i+1] == 'j':
                 kor_str += kor_index[eng2krindex['nj']]
                 flag = True
@@ -60,58 +60,103 @@ def eng_to_kor(s):
                 flag = True
             else:
                 kor_str += kor_index[eng2krindex['n']]
-        elif s[i] == 'm':
+        elif s[i] == 'm' and i < len(s)-1:
             if s[i+1] == 'l':
                 kor_str += kor_index[eng2krindex['ml']]
                 flag = True
             else:
                 kor_str += kor_index[eng2krindex['m']]
         # 여기부터 쌍자음 받침 처리
-        elif s[i] == 'r' and s[i+2] in consonant and i<len(s)-2:
-            if s[i+1] == 't':
-                kor_str += kor_index[eng2krindex['rt']]
-                flag = True
+        if s[i] == 'r':
+            if i < len(s)-2:
+                if s[i+1] == 't' and s[i+2] in consonant:
+                    kor_str += kor_index[eng2krindex['rt']]
+                    flag = True
+                else:
+                    kor_str += kor_index[eng2krindex['r']]
             else:
-                kor_str += kor_index[eng2krindex['r']]
-        elif s[i] == 's' and s[i+2] in consonant:
-            if s[i+1] == 'w':
-                kor_str += kor_index[eng2krindex['sw']]
-                flag = True
-            elif s[i+1] == 'g':
-                kor_str += kor_index[eng2krindex['sg']]
-                flag = True
+                if s[i+1] == 't':
+                    kor_str += kor_index[eng2krindex['rt']]
+                    flag = True
+        elif s[i] == 's':
+            if i < len(s)-2:
+                if s[i+1] == 'w' and s[i+2] in consonant:
+                    kor_str += kor_index[eng2krindex['sw']]
+                    flag = True
+                elif s[i+1] == 'g' and s[i+2] in consonant:
+                    kor_str += kor_index[eng2krindex['sg']]
+                    flag = True
+                else:
+                    kor_str += kor_index[eng2krindex['s']]
             else:
-                kor_str += kor_index[eng2krindex['s']]
-        elif s[i] == 'f' and s[i+2] in consonant:
-            if s[i+1] == 'r':
-                kor_str += kor_index[eng2krindex['fr']]
-                flag = True
-            elif s[i+1] == 'a':
-                kor_str += kor_index[eng2krindex['fa']]
-                flag = True
-            elif s[i+1] == 'q':
-                kor_str += kor_index[eng2krindex['fq']]
-                flag = True
-            elif s[i+1] == 't':
-                kor_str += kor_index[eng2krindex['ft']]
-                flag = True
-            elif s[i+1] == 'x':
-                kor_str += kor_index[eng2krindex['fx']]
-                flag = True
-            elif s[i+1] == 'v':
-                kor_str += kor_index[eng2krindex['fv']]
-                flag = True
-            elif s[i+1] == 'g':
-                kor_str += kor_index[eng2krindex['fg']]
-                flag = True
+                if s[i+1] == 'w':
+                    kor_str += kor_index[eng2krindex['sw']]
+                    flag = True
+                elif s[i+1] == 'g':
+                    kor_str += kor_index[eng2krindex['sg']]
+                    flag = True
+                else:
+                    kor_str += kor_index[eng2krindex['s']]
+        elif s[i] == 'f':
+            if i < len(s)-2:
+                if s[i+1] == 'r' and s[i+2] in consonant:
+                    kor_str += kor_index[eng2krindex['fr']]
+                    flag = True
+                elif s[i+1] == 'a' and s[i+2] in consonant:
+                    kor_str += kor_index[eng2krindex['fa']]
+                    flag = True
+                elif s[i+1] == 'q' and s[i+2] in consonant:
+                    kor_str += kor_index[eng2krindex['fq']]
+                    flag = True
+                elif s[i+1] == 't' and s[i+2] in consonant:
+                    kor_str += kor_index[eng2krindex['ft']]
+                    flag = True
+                elif s[i+1] == 'x' and s[i+2] in consonant:
+                    kor_str += kor_index[eng2krindex['fx']]
+                    flag = True
+                elif s[i+1] == 'v' and s[i+2] in consonant:
+                    kor_str += kor_index[eng2krindex['fv']]
+                    flag = True
+                elif s[i+1] == 'g' and s[i+2] in consonant:
+                    kor_str += kor_index[eng2krindex['fg']]
+                    flag = True
+                else:
+                    kor_str += kor_index[eng2krindex['f']]
             else:
-                kor_str += kor_index[eng2krindex['f']]
-        elif s[i] == 'q' and s[i+2] in consonant:
-            if s[i+1] == 't':
-                kor_str += kor_index[eng2krindex['qt']]
-                flag = True
+                if s[i+1] == 'r':
+                    kor_str += kor_index[eng2krindex['fr']]
+                    flag = True
+                elif s[i+1] == 'a':
+                    kor_str += kor_index[eng2krindex['fa']]
+                    flag = True
+                elif s[i+1] == 'q':
+                    kor_str += kor_index[eng2krindex['fq']]
+                    flag = True
+                elif s[i+1] == 't':
+                    kor_str += kor_index[eng2krindex['ft']]
+                    flag = True
+                elif s[i+1] == 'x':
+                    kor_str += kor_index[eng2krindex['fx']]
+                    flag = True
+                elif s[i+1] == 'v':
+                    kor_str += kor_index[eng2krindex['fv']]
+                    flag = True
+                elif s[i+1] == 'g':
+                    kor_str += kor_index[eng2krindex['fg']]
+                    flag = True
+                else:
+                    kor_str += kor_index[eng2krindex['f']]
+        elif s[i] == 'q':
+            if i < len(s)-2:
+                if s[i+1] == 't' and s[i+2] in consonant:
+                    kor_str += kor_index[eng2krindex['qt']]
+                    flag = True
+                else:
+                    kor_str += kor_index[eng2krindex['q']]
             else:
-                kor_str += kor_index[eng2krindex['q']]
+                if s[i+1] == 't':
+                    kor_str += kor_index[eng2krindex['qt']]
+                    flag = True
         else:
             kor_str += kor_index[eng2krindex[s[i]]]
     
